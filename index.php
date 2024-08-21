15451e515a55378c3cb9e1b0223a10a84cab9492
Your Name <favian.mustafa13@gmail.com>
2024-08-21 08:07:21 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+// koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "ujicoba");
+// ambil data sintesa dari data sintesa
+$result = mysqli_query($conn, "SELECT * FROM ujicoba");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman admin</title>
+</head>
+<body>
+    <h1>Daftar sintesa</h1>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Nama</th>
+            <th>Alamat</th>
+        </tr>
+
+        <?php while ( $row = mysqli_fetch_assoc($result) ) :
+        ?>
+        <tr>
+            <td><?= $row["id"]?></td>
+            <td><?= $row["nama"]; ?></td>
+            <td><?= $row["alamat"]; ?></td>
+        </tr>
+        <?php endwhile; ?>
+    </table>
+</body>
+</html>
